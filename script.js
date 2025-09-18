6a8489829698031c09b19d81a47aabfd53bf6825
Sandeep Yadav <keeponn87@gmail.com>
2025-09-19 00:56:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-const typingText = document.querySelector(".typing-text");
-const textArray = ["Data Analyst", "SQL Developer", "Python Enthusiast", "Dashboard Creator"];
-let index = 0;
+const textArray = ["Data Analyst", "Web Developer", "Problem Solver"];
+let typingElement = document.querySelector(".typing-text");
+let arrayIndex = 0;
 let charIndex = 0;
 
-function type() {
-  if (charIndex < textArray[index].length) {
-    typingText.textContent += textArray[index].charAt(charIndex);
+function typeEffect() {
+  if (charIndex < textArray[arrayIndex].length) {
+    typingElement.textContent += textArray[arrayIndex].charAt(charIndex);
     charIndex++;
-    setTimeout(type, 100);
+    setTimeout(typeEffect, 100);
   } else {
-    setTimeout(erase, 1500);
+    setTimeout(eraseEffect, 1500);
   }
 }
 
-function erase() {
+function eraseEffect() {
   if (charIndex > 0) {
-    typingText.textContent = textArray[index].substring(0, charIndex - 1);
+    typingElement.textContent = textArray[arrayIndex].substring(0, charIndex - 1);
     charIndex--;
-    setTimeout(erase, 50);
+    setTimeout(eraseEffect, 50);
   } else {
-    index = (index + 1) % textArray.length;
-    setTimeout(type, 200);
+    arrayIndex = (arrayIndex + 1) % textArray.length;
+    setTimeout(typeEffect, 500);
   }
 }
 
-document.addEventListener("DOMContentLoaded", type);
+document.addEventListener("DOMContentLoaded", () => {
+  typeEffect();
+});
